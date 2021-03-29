34adf164f701924cf92c6e4e38392da8f9125eae
xLEANDROx <rota116@gmail.com>
2021-03-29 17:42:28 -0300

    Definir movimentos

--- js/script.js
+++ js/script.js
@@ -4,8 +4,10 @@ let box = 32;
 let snake = [];
 snake[0] = {
     x: 8 * box,
-    y: 8 *box,
+    y: 8 * box,
 }
+
+let direction = "right";
 
 function criarBg() {
     context.fillStyle = "lightgreen";
@@ -14,12 +16,35 @@ function criarBg() {
 function criarCobrinha() {
     for (i=0; i < snake.length; i++){
         context.fillStyle="green";
-        context.fillRect (snake[i].x, snake[i].y, box, box,);
+        context.fillRect(snake[i].x, snake[i].y, box, box,);
     }
 
 }
- criarBg();
- criarCobrinha();
+
+function iniciarJogo(){
+    criarBg();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if (direction == "right") snakeX += box;
+    if (direction == "left") snakeX -= box;
+    if (direction == "up") snakeY += box;
+    if (direction == "down") snakeY -= box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY    
+    }
+
+    snake.unshift(newHead);
+
+}
+
+let jogo = setInterval(iniciarJogo, 100); 
 
 
 
